febe74357128c11824819e61cb8415acef66627c
mt-kenny <ktsuji+mt@moneytree.jp>
2019-07-04 15:15:45 +0900

    refactor

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ require('dotenv').config()
 const Slack = require('slack');
 const slack = new Slack();
 
-const allSelections = ['パンダ', 'コアラ', 'タスマニアタイガー', 'ディンゴ', 'タスマニアデビル', 'キリン', 'ゾウ'];
-const maxSizeOfEachAction = 5;
-const message = 'pick!';
+const categories = ['パンダ', 'コアラ', 'タスマニアタイガー', 'ディンゴ', 'タスマニアデビル', 'キリン', 'ゾウ'];
+const MAX_ACTIONS = 5;
+const message = 'pick one!';
 
 const getAttachments = function(list, divideLength) {
   const result = [];
   let count = 0;
 
   for (let i = 0; i < list.length; i += divideLength) {
-    const splittedList = list.slice(i, i + divideLength);
+    const splitted = list.slice(i, i + divideLength);
     const attachment = {
       callback_id: `selection_${count++}`,
       text: '',
-      actions: splittedList.map(v => ({
+      actions: splitted.map(v => ({
         type: 'button',
         text: v,
         name: v
@@ -29,11 +29,9 @@ const getAttachments = function(list, divideLength) {
   return result;
 }
 
-const attachmentsToUse = getAttachments(allSelections, maxSizeOfEachAction);
-
 slack.chat.postMessage({
   token: process.env.SLACK_BOT_TOKEN,
   channel: process.env.SLACK_CHANNEL,
   text: message,
-  attachments: attachmentsToUse
+  attachments: getAttachments(categories, MAX_ACTIONS)
 }).then(console.log).catch(console.error);
