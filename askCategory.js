4ac2371741359e195c048213eb65579bee2254c7
mt-kenny <ktsuji+mt@moneytree.jp>
2019-07-04 15:22:13 +0900

    enable to set callback prefix

--- askCategory.js
+++ askCategory.js
@@ -6,15 +6,16 @@ const slack = new Slack();
 const categories = ['パンダ', 'コアラ', 'タスマニアタイガー', 'ディンゴ', 'タスマニアデビル', 'キリン', 'ゾウ'];
 const MAX_ACTIONS = 5;
 const message = 'pick one!';
+const callback_prefix = 'category_pick_';
 
-const getAttachments = function(list, divideLength) {
+const getAttachments = function(list, callbackPrefix, divideLength) {
   const result = [];
   let count = 0;
 
   for (let i = 0; i < list.length; i += divideLength) {
     const splitted = list.slice(i, i + divideLength);
     const attachment = {
-      callback_id: `selection_${count++}`,
+      callback_id: `${callbackPrefix}${count++}`,
       text: '',
       actions: splitted.map(v => ({
         type: 'button',
@@ -33,5 +34,5 @@ slack.chat.postMessage({
   token: process.env.SLACK_BOT_TOKEN,
   channel: process.env.SLACK_CHANNEL,
   text: message,
-  attachments: getAttachments(categories, MAX_ACTIONS)
+  attachments: getAttachments(categories, callback_prefix, MAX_ACTIONS)
 }).then(console.log).catch(console.error);
