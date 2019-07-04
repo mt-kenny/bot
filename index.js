df0decbe2f8577c85d58a62f2744aa6293f2bf72
mt-kenny <ktsuji+mt@moneytree.jp>
2019-07-04 14:26:25 +0900

    add selections

--- index.js
+++ index.js
@@ -3,17 +3,32 @@ require('dotenv').config()
 const Slack = require('slack');
 const slack = new Slack();
 
+const message = 'zoo selection';
+const selections = ['パンダ1', 'コアラ', 'タスマニアタイガー', 'ディンゴ', 'タスマニアデビル']; 
+const selections2 = ['パンダ2', 'コアラ', 'タスマニアタイガー', 'ディンゴ', 'タスマニアデビル']; 
+
 slack.chat.postMessage({
     token: process.env.SLACK_BOT_TOKEN,
     channel: process.env.SLACK_CHANNEL,
-    text: '動物園',
-    attachments: [{
+    text: message,
+    attachments: [
+      {
         callback_id: 'animals_button',
         text: '',
-        actions: ['パンダ', 'コアラ', 'タスマニアタイガー', 'ディンゴ', 'タスマニアデビル'].map(v => ({
+        actions: selections.map(v => ({
             type: 'button',
             text: v,
             name: v
         }))
-    }]
+      },
+      {
+        callback_id: 'animals_button2',
+        text: '',
+        actions: selections2.map(v => ({
+            type: 'button',
+            text: v,
+            name: v
+        }))
+      }
+    ],
 }).then(console.log).catch(console.error);
